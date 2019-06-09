3addf384335fea6336890a4df25d257eb9018ef8
Michael <michael@MichaeldeMacBook-Pro.local>
2019-06-09 11:39:22 +0800

    update

---
+++ src/main/java/com/example/concurrentspecialsubject/ConcurrentSpecialSubjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrentspecialsubject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentSpecialSubjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentSpecialSubjectApplication.class, args);
+    }
+
+}
