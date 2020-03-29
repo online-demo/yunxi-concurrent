b21302656500dfa52a7a73abf3432daac7b27ed8
云析学院无双老师 <1102820281@qq.com>
2020-03-29 22:34:29 +0800

    update

--- 25-atomic/src/main/java/com/example/atomic/atomicinteger/demo/AtomicIntegerDemo.java
+++ 25-atomic/src/main/java/com/example/atomic/atomicinteger/demo/AtomicIntegerDemo.java
@@ -4,7 +4,17 @@ import	java.util.concurrent.atomic.AtomicInteger;
 /**
  * @Author: 无双老师【云析学院:http://yunxiedu.net QQ:3190976240 email:zhouguanya20@163.com】
  * @Date: 2020-03-21 16:20
- * @Description: AtomicInteger使用方式
+ * @Description: 无双老师自我简介
+ *
+ * 昵称：无双（借鉴周杰伦游戏歌曲《无双》）
+ *
+ * 清华大学出版社作家，上海交通大学软件工程专业硕士。
+ *
+ * 10年以上开发经验，具备多个大规模互联网项目实战经验。
+ *
+ * 代表作《Spring5企业级开发实战》https://item.jd.com/12641910.html#crumb-wrap
+ *
+ * 精通简历，面试，Java，熟悉高并发，分布式技术，对MySQL/Oracle等主流数据库的优化有丰富的经验。
  */
 public class AtomicIntegerDemo {
 
